518a84c7c244b2f530341a238d45b97f39a8b6fc
Al-Amin-Khan-Shakil <alaminkhanshakil@gmail.com>
2024-05-07 12:44:18 +0600

    Set formated time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+function formatTime(date) {
+  return new Intl.DateTimeFormat('en', {
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(date);
+}
+
 function App() {
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(formatTime(new Date()));
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <main>
+      <h1>Workout Timer</h1>
+      <time>
+        For your workout on
+        {' '}
+        {time}
+      </time>
+    </main>
   );
 }
 
